c051c9f5752fa5ae7568a27bdf772aa96d923406
AaronDownward <downward150@gmail.com>
2020-11-05 14:23:49 -0600

    removing commented part, giving up for now

--- pipelines/dockerized-maven.groovy
+++ pipelines/dockerized-maven.groovy
@@ -25,89 +25,89 @@ pipeline {
                 }
             }
         }
-        // stage('Build') {
-        //     agent {
-        //         docker { 
-        //             image 'maven'
-        //             args '-v $HOME/.m2:/root/.m2 --net=host' }
-        //     }
-        //     steps {
-        //         dir("maven-app") {
-        //             sh "mvn package"
-        //         }
-        //     }
-        //     post {
-        //         failure{
-        //             slackSend(color: '#FF0000', message: "${projectName}' [${gitBranch}:${currentBuild.number}] has failed to build.")
-        //         }
-        //     }
-        // }
-        // stage("Sonar") {
-        //     agent {
-        //         docker { 
-        //             image 'maven'
-        //             args '-v $HOME/.m2:/root/.m2 --net=host' }
-        //     }
-        //     steps {
-        //         dir("maven-app") {
-        //             withSonarQubeEnv("SonarCloud") {
-        //                 sh "mvn verify sonar:sonar"
-        //             }
-        //         }
-        //     }
-        //     post {
-        //         failure{
-        //             slackSend(color: '#FF0000', message: "${projectName}' [${gitBranch}:${currentBuild.number}] has failed to run sonar tests.")
-        //         }
-        //     }
-        // }
-        // stage('quality gate'){
-        //     agent {
-        //         docker { 
-        //             image 'maven'
-        //             args '-v $HOME/.m2:/root/.m2 --net=host' }
-        //     }
-        //     steps{
-        //         dir("maven-app") {
-        //             // withSonarQubeEnv('SonarCloud')
-        //             // {
-        //             //     timeout(time: 10, unit: 'MINUTES') {
-        //             //         // Parameter indicates whether to set pipeline to UNSTABLE if Quality Gate fails
-        //             //         // true = set pipeline to UNSTABLE, false = don't
-        //             //         waitForQualityGate abortPipeline: false
-        //             //     }
-        //             // }
-        //             echo 'pass'
-        //         }
-        //     }
-        //     post {
-        //         failure{
-        //             slackSend(color: '#FF0000', message: "${projectName}' [${gitBranch}:${currentBuild.number}] did not pass the quality gate")
-        //         }
-        //     }
-        // }
-        // stage("Build Docker Image") {
-        //     agent {
-        //         docker { image 'docker' }
-        //     }
-        //     steps {
-        //         script {
-        //             dir("maven-app") {
-        //                 docker.withTool("docker") {
-        //                     repoIdCurrent = "aarondownward/maven-app:${currentBuild.number}"
-        //                     repoIdLatest = "aarondownward/maven-app:latest"
-        //                     dockerImageCurrent = docker.build(repoIdCurrent)
-        //                     dockerImageLatest = docker.build(repoIdLatest)
-        //                 }
-        //             }
-        //         }
-        //     }
-        //     post {
-        //         failure{
-        //             slackSend(color: '#FF0000', message: "${projectName}' [${gitBranch}:${currentBuild.number}] has failed to build docker image.")
-        //         }
-        //     }
-        // }
+        stage('Build') {
+            agent {
+                docker { 
+                    image 'maven'
+                    args '-v $HOME/.m2:/root/.m2 --net=host' }
+            }
+            steps {
+                dir("maven-app") {
+                    sh "mvn package"
+                }
+            }
+            post {
+                failure{
+                    slackSend(color: '#FF0000', message: "${projectName}' [${gitBranch}:${currentBuild.number}] has failed to build.")
+                }
+            }
+        }
+        stage("Sonar") {
+            agent {
+                docker { 
+                    image 'maven'
+                    args '-v $HOME/.m2:/root/.m2 --net=host' }
+            }
+            steps {
+                dir("maven-app") {
+                    withSonarQubeEnv("SonarCloud") {
+                        sh "mvn verify sonar:sonar"
+                    }
+                }
+            }
+            post {
+                failure{
+                    slackSend(color: '#FF0000', message: "${projectName}' [${gitBranch}:${currentBuild.number}] has failed to run sonar tests.")
+                }
+            }
+        }
+        stage('quality gate'){
+            agent {
+                docker { 
+                    image 'maven'
+                    args '-v $HOME/.m2:/root/.m2 --net=host' }
+            }
+            steps{
+                dir("maven-app") {
+                    // withSonarQubeEnv('SonarCloud')
+                    // {
+                    //     timeout(time: 10, unit: 'MINUTES') {
+                    //         // Parameter indicates whether to set pipeline to UNSTABLE if Quality Gate fails
+                    //         // true = set pipeline to UNSTABLE, false = don't
+                    //         waitForQualityGate abortPipeline: false
+                    //     }
+                    // }
+                    echo 'pass'
+                }
+            }
+            post {
+                failure{
+                    slackSend(color: '#FF0000', message: "${projectName}' [${gitBranch}:${currentBuild.number}] did not pass the quality gate")
+                }
+            }
+        }
+        stage("Build Docker Image") {
+            agent {
+                docker { image 'docker' }
+            }
+            steps {
+                script {
+                    dir("maven-app") {
+                        docker.withTool("docker") {
+                            repoIdCurrent = "aarondownward/maven-app:${currentBuild.number}"
+                            repoIdLatest = "aarondownward/maven-app:latest"
+                            dockerImageCurrent = docker.build(repoIdCurrent)
+                            dockerImageLatest = docker.build(repoIdLatest)
+                        }
+                    }
+                }
+            }
+            post {
+                failure{
+                    slackSend(color: '#FF0000', message: "${projectName}' [${gitBranch}:${currentBuild.number}] has failed to build docker image.")
+                }
+            }
+        }
         stage('Scan image') {
             agent {
                 docker { 
@@ -128,60 +128,49 @@ pipeline {
                 }
             }
         }
-        stage('printEnv') {
+        stage("Docker push") {
             agent {
-                docker { 
-                    image 'aarondownward/trivy'
-                    args '--net=host'
-                    }
+                docker { image 'docker' }
             }
             steps {
-                echo "${trivyCode}"
+                script {
+                    docker.withTool("docker") {
+                        docker.withRegistry("https://registry.hub.docker.com", "dockerhub-cred") {
+                            dockerImageCurrent.push()
+                            dockerImageLatest.push()
+                        }
+                    }
+                }
+            }
+            post {
+                failure{
+                    slackSend(color: '#FF0000', message: "${projectName}' [${gitBranch}:${currentBuild.number}] has failed to push image to docker hub.")
+                }
             }
         }
-        // stage("Docker push") {
-        //     agent {
-        //         docker { image 'docker' }
-        //     }
-        //     steps {
-        //         script {
-        //             docker.withTool("docker") {
-        //                 docker.withRegistry("https://registry.hub.docker.com", "dockerhub-cred") {
-        //                     dockerImageCurrent.push()
-        //                     dockerImageLatest.push()
-        //                 }
-        //             }
-        //         }
-        //     }
-        //     post {
-        //         failure{
-        //             slackSend(color: '#FF0000', message: "${projectName}' [${gitBranch}:${currentBuild.number}] has failed to push image to docker hub.")
-        //         }
-        //     }
-        // }
-        // stage("Deploy to Testing") {
-        //     agent {
-        //         docker { 
-        //             image 'reblank/kubectl_agent' 
-        //             args '--net=host'}
-        //     }
-        //     steps {
-        //         dir("kube-deployments/testing") {
-        //             withKubeConfig([credentialsId: 'kubectl-creds', serverUrl: 'https://EF62A4A1E3BA2F867AF5F946B1E0B21B.gr7.us-west-2.eks.amazonaws.com']) {
-        //                 sh 'kubectl apply -f .'
-        //                 sh 'kubectl rollout restart deployment/simple-app -n build'
-        //             }
-        //         }
-        //     }
-        //     post {
-        //         success{
-        //             slackSend(color: '#00FF00', message: "${projectName}' [${gitBranch}:${currentBuild.number}] successfully completed CI/CD pipeline.")
-        //         }
-        //         failure{
-        //             slackSend(color: '#FF0000', message: "${projectName}' [${gitBranch}:${currentBuild.number}] has failed to deploy to test server.")
-        //         }
-        //     }
-        // }
+        stage("Deploy to Testing") {
+            agent {
+                docker { 
+                    image 'reblank/kubectl_agent' 
+                    args '--net=host'}
+            }
+            steps {
+                dir("kube-deployments/testing") {
+                    withKubeConfig([credentialsId: 'kubectl-creds', serverUrl: 'https://EF62A4A1E3BA2F867AF5F946B1E0B21B.gr7.us-west-2.eks.amazonaws.com']) {
+                        sh 'kubectl apply -f .'
+                        sh 'kubectl rollout restart deployment/simple-app -n build'
+                    }
+                }
+            }
+            post {
+                success{
+                    slackSend(color: '#00FF00', message: "${projectName}' [${gitBranch}:${currentBuild.number}] successfully completed CI/CD pipeline.")
+                }
+                failure{
+                    slackSend(color: '#FF0000', message: "${projectName}' [${gitBranch}:${currentBuild.number}] has failed to deploy to test server.")
+                }
+            }
+        }
     }
 
 }
